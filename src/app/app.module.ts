31cee3aec3c3f19340e16a545d2b585726d82fc4
VartanovMaks <vartanov2009@gmail.com>
2021-06-04 11:27:49 +0300

    user-users iteration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
-import {HttpClientModule } from '@angular/common/http'
+import {HttpClientModule } from '@angular/common/http';
+import { UserComponent } from './components/user/user.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,17 @@
+import { User } from './../../models/user';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+  @Input()
+  user:User;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
