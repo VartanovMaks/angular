eee7548830192784d72f1d063fb6a46d67e21792
VartanovMaks <vartanov2009@gmail.com>
2021-06-04 13:49:56 +0300

    children route. 20min

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule } from '@angular/common/http';
@@ -7,22 +7,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { UserComponent } from './components/user/user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { HomeComponent } from './components/home/home.component';
 
+let routes:Routes = [
+  { path:'home', component:HomeComponent},
+  { path:'users', component:UsersComponent,
+    children:[{path:':id', component:UserDetailsComponent}]
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     UserComponent,
+    UserDetailsComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path:'home', component:HomeComponent},
-      { path:'users', component:UsersComponent}
-    ]),
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,6 @@
 import { User } from './../../models/user';
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -9,13 +10,16 @@ import { Component, Input, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   @Input()
   user:User;
-  constructor() { }
+  constructor(private router: Router, private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
   }
   goToDetails():void{
-    console.log(this.user);
     // /users/:id
+    console.log(this.user);
+
+    this.router.navigate([this.user.id], {relativeTo:this.activatedRoute});
+    // this.router.navigate(['users',this.user.id], {relativeTo:this.activatedRoute});
 
 
   }
